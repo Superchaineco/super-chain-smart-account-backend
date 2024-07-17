f3a0ea89089c28af3d8d734ef13805a61fe613f4
AnotherDev <joseluismanco37@gmail.com>
2024-07-17 16:04:02 +0300

    feat: update smart contracts

--- src/config/superChain/constants.ts
+++ src/config/superChain/constants.ts
@@ -1,11 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import SuperChainSetupABI from './abi/SuperChainSetup.json';
 import SuperChainModuleABI from './abi/SuperChainModule.json';
 export const SUPER_CHAIN_MODULE_ABI = SuperChainModuleABI;
-export const SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS = '0x20e671bB7285AECA2f7d318cd4F5Aa61041A2e5F'
-export const SUPER_CHAIN_ATTESTATION_SCHEMA = '0x0c3f4c1ad0bc7048f405838bd315e8e64c2ab4cd71d06209e47d9d952da66b3a'
+export const SUPER_CHAIN_ACCOUNT_MODULE_ADDRESS = '0x47BE9A404B531A224c2118bc10a007350e586560'
+export const SUPER_CHAIN_ATTESTATION_SCHEMA = '0xce12246c39b440c01a6fefb334a235bd1f04673b0279d5055d98f5869246fd1b'
 export const JSON_RPC_PROVIDER = process.env.JSON_RPC_PROVIDER
 
 export const EAS_CONTRACT_ADDRESS =
