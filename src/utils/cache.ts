a087b2d3245870069a5918319e01b04cac28b5e6
AnotherDev <joseluismanco37@gmail.com>
2024-08-13 13:16:58 -0300

    chore: test deployment config

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 import { REDIS_URL } from "../config/superChain/constants";
-
+console.debug("REDIS_URL", REDIS_URL);
 const redis = new Redis(REDIS_URL);
 
 export { redis };
