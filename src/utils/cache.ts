73ba07ff5fc2927759a0702d70535f3ff1c9fb0b
AnotherDev <joseluismanco37@gmail.com>
2024-08-13 14:13:28 -0300

    fix: redis

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,6 +1,18 @@
 import Redis from "ioredis";
-import { REDIS_URL } from "../config/superChain/constants";
+import {
+  REDIS_URL,
+  REDIS_PASSWORD,
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_USER,
+} from "../config/superChain/constants";
 console.debug("REDIS_URL", REDIS_URL);
-const redis = new Redis(REDIS_URL);
+const redis = new Redis({
+  host: REDIS_HOST,
+  port: REDIS_PORT,
+  username: REDIS_USER,
+  password: REDIS_PASSWORD,
+  family: 0,
+});
 
 export { redis };
