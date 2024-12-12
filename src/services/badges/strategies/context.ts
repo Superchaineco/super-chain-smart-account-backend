76e056622bf920e8dc2b2b20d7c985468a16fa50
anotherdev <joseluismanco37@gmail.com>
2024-12-12 13:49:20 -0500

    feat: add strategies to backend

--- src/services/badges/strategies/context.ts
+++ src/services/badges/strategies/context.ts
@@ -2,8 +2,11 @@
 import { BadgeStrategy } from './badgeStrategy';
 import { BaseTransactionsStrategy } from './baseTransactions';
 import { CitizenCheckStrategy } from './citizenCheck';
+import { EarlyAdoptersStrategy } from './EarlyAdopters';
 import { GitcoinDonationsStrategy } from './gitcoinDonations';
 import { GivethDonationsStrategy } from './givethDonations';
+import { LiskBuildGuildAirdropStrategy } from './liskBuildGuildAirdrop';
+import { LiskTransactionsStrategy } from './liskTransactions';
 import { ModeTransactionsStrategy } from './modeTransactions';
 import { NounsCheckStrategy } from './nounsCheck';
 import { OpTransactionsStrategy } from './opTransactions';
@@ -45,6 +48,12 @@ export class BadgeStrategyContext {
                 return new WorldCoinCheckStrategy()
             case "Super Cohort 24":
                 return new SuperCohort0Strategy()
+            case "Lisk User":
+                return new LiskTransactionsStrategy()
+            case "Lisk Aidrop S1":
+                return new LiskBuildGuildAirdropStrategy()
+            case "Early Power user":
+                return new EarlyAdoptersStrategy()
 
             default:
                 throw new Error(`Badge strategy ${badgeName} not found`);
