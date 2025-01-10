6347b13f693270e1102e6b2c6e3dd22323e3e618
anotherdev <joseluismanco37@gmail.com>
2025-01-09 20:16:08 -0500

    chore: add try catch to handle general errors

--- src/services/badges/strategies/badgeStrategy.ts
+++ src/services/badges/strategies/badgeStrategy.ts
@@ -1,48 +1,64 @@
 import { Badge, ResponseBadge } from '../badges.service';
 export interface BadgeStrategy {
-    calculateTier(eoas: string[], badgeData: Badge): Promise<ResponseBadge>;
+  calculateTier(eoas: string[], badgeData: Badge, account?: string): Promise<ResponseBadge>;
 }
 
 export abstract class BaseBadgeStrategy implements BadgeStrategy {
-    abstract getValue(eoas: string[], account?: string): Promise<number | boolean>;
+  abstract getValue(
+    eoas: string[],
+    account?: string
+  ): Promise<number | boolean>;
 
-    async calculateTier(eoas: string[], badgeData: Badge, account?: string): Promise<ResponseBadge> {
-        const value = await this.getValue(eoas, account);
+  async calculateTier(
+    eoas: string[],
+    badgeData: Badge,
+    account?: string
+  ): Promise<ResponseBadge> {
+    try {
+      const value = await this.getValue(eoas, account);
 
-        let claimableTier: number | null = null;
-        let claimable = false;
+      let claimableTier: number | null = null;
+      let claimable = false;
 
-        if (typeof value === 'number') {
-            claimableTier = this.calculateNumericTier(badgeData, value);
-            claimable = claimableTier ? badgeData.tier < claimableTier : false;
-        } else if (typeof value === 'boolean') {
-            claimableTier = value ? 1 : null;
-            claimable = value ? badgeData.tier != 1 : false;
-        }
+      if (typeof value === 'number') {
+        claimableTier = this.calculateNumericTier(badgeData, value);
+        claimable = claimableTier ? badgeData.tier < claimableTier : false;
+      } else if (typeof value === 'boolean') {
+        claimableTier = value ? 1 : null;
+        claimable = value ? badgeData.tier != 1 : false;
+      }
 
-        if (claimableTier !== null && claimableTier < Number(badgeData.tier)) {
-            claimableTier = Number(badgeData.tier);
-        }
+      if (claimableTier !== null && claimableTier < Number(badgeData.tier)) {
+        claimableTier = Number(badgeData.tier);
+      }
 
-        return {
-            ...badgeData.badge,
-            points: badgeData.points,
-            tier: badgeData.tier,
-            claimableTier,
-            claimable,
-        };
+      return {
+        ...badgeData.badge,
+        points: badgeData.points,
+        tier: badgeData.tier,
+        claimableTier,
+        claimable,
+      };
+    } catch (error) {
+      console.error('Error calculating tier', error);
+      throw error;
     }
+  }
 
-    protected calculateNumericTier(badgeData: Badge, value: number): number {
-        const badgeTiers = badgeData.badge.badgeTiers;
-        if (!badgeTiers) throw new Error('No tiers found for badge');
+  protected calculateNumericTier(badgeData: Badge, value: number): number {
+    try {
+      const badgeTiers = badgeData.badge.badgeTiers;
+      if (!badgeTiers) throw new Error('No tiers found for badge');
 
-        for (let i = badgeTiers.length - 1; i >= 0; i--) {
-            if (value >= badgeTiers[i].metadata!.minValue) {
-                return i + 1;
-            }
-        }
-        return 0;
+    for (let i = badgeTiers.length - 1; i >= 0; i--) {
+      if (value >= badgeTiers[i].metadata!.minValue) {
+        return i + 1;
+      }
     }
+      return 0;
+    } catch (error) {
+      console.error('Error calculating numeric tier', error);
+      throw error;
+    }
+  }
 }
-
--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -2,18 +2,23 @@ import { redis } from "../utils/cache";
 
 export class RedisService {
     public async getCachedDataWithCallback<T>(key: string, fetchFunction: () => Promise<T>, ttl: number): Promise<T> {
-        const cachedData = await redis.get(key);
-        if (cachedData) {
-            console.info(`Cache hit for key: ${key}`);
+        try {
+            const cachedData = await redis.get(key);
+            if (cachedData) {
+                console.info(`Cache hit for key: ${key}`);
             return JSON.parse(cachedData);
         }
 
         const data = await fetchFunction();
 
         if (ttl > 0) {
-            await redis.set(key, JSON.stringify(data), "EX", ttl);
+                await redis.set(key, JSON.stringify(data), "EX", ttl);
+            }
+            return data;
+        } catch (error) {
+            console.error('Error getting cached data', error);
+            throw error;
         }
-        return data;
     }
 
     public async setCachedData(key: string, data: any, ttl: number) {
