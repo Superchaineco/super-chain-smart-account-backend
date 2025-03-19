5f1c45b7fb154316961b1ad75cfcb71703c37a87
sebaxor <sebasor@gmail.com>
2025-03-19 06:20:41 -0500

    feat(season badge): season 7 badge

--- src/services/badges/strategies/context.ts
+++ src/services/badges/strategies/context.ts
@@ -14,9 +14,11 @@ import { ModeTransactionsStrategy } from './modeTransactions';
 import { NounsCheckStrategy } from './nounsCheck';
 import { OpTransactionsStrategy } from './opTransactions';
 import { SoneiumTransactionsStrategy } from './soneiumTransactions';
+import { SuperChainTransactionsStrategy } from './superchainTransactions';
 import { SuperCohort0Strategy } from './superCohort0Strategy';
 import { SwellTransactionsStrategy } from './swellTransactions';
 import { TalentScoreStrategy } from './talentScore';
+import { UnichainTransactionsStrategy } from './unichainTransactions';
 import { WorldCoinCheckStrategy } from './worldCoinCheck';
 
 
@@ -55,14 +57,18 @@ export class BadgeStrategyContext {
                 return new SoneiumTransactionsStrategy()
             case "Ink User":
                 return new InkTransactionsStrategy()
-            case "Mint User":
-                return new MintTransactionsStrategy()
-            case "Swell User":
-                return new SwellTransactionsStrategy()
-            case "Metal User":
-                return new MetalTransactionsStrategy()
-            case "Mode User":
-                return new ModeTransactionsStrategy()
+            case "Unichain User":
+                return new UnichainTransactionsStrategy()
+            case "Season Super User":
+                return new SuperChainTransactionsStrategy()
+
+            //NOT RELEVANT FOR NOW
+            // case "Mint User":
+            //     return new MintTransactionsStrategy()
+            // case "Swell User":
+            //     return new SwellTransactionsStrategy()
+            // case "Metal User":
+            //     return new MetalTransactionsStrategy()
 
             default:
                 throw new Error(`Badge strategy ${badgeName} not found`);
