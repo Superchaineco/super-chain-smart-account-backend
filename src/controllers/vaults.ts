13e79af5aa9e483513e9c2056f9b8f2fec869b29
anotherdev <joseluismanco37@gmail.com>
2025-04-29 12:30:52 -0500

    feat: cache for vaults APR

--- src/controllers/vaults.ts
+++ src/controllers/vaults.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
-import { getVaultsAPR, refreshVaultsCache } from "../services/vaults.service";
+import { VaultsService } from "../services/vaults.service";
+import { redisService } from "../services/redis.service";
 
 export async function getVaults(req: Request, res: Response) {
   const account = req.params.account as string;
-  const vaults = await getVaultsAPR(account);
+  const vaultsService = new VaultsService(redisService);
+  const vaults = await vaultsService.getVaultsAPR(account);
   res.status(200).json(vaults);
 }
 
 export async function refreshVaults(req: Request, res: Response) {
   const account = req.params.account as string;
-  await refreshVaultsCache(account)
+  const vaultsService = new VaultsService(redisService);
+  await vaultsService.refreshVaultsCache(account)
   res.status(200)
 }
 
--- src/services/vaults.service.ts
+++ src/services/vaults.service.ts
@@ -1,7 +1,7 @@
 import { JSON_RPC_PROVIDER } from '@/config/superChain/constants';
 import axios from 'axios';
 import { Contract, JsonRpcProvider } from 'ethers';
-import { redisService } from './redis.service';
+import { RedisService } from './redis.service';
 
 const tokenImages = {
   ETH: 'https://staging.account.superchain.eco/images/vaults/icons/ETH_OP.svg',
@@ -9,80 +9,55 @@ const tokenImages = {
   USDT: 'https://staging.account.superchain.eco/images/vaults/icons/USDT_OP.svg'
 };
 
-async function getVaultsData() {
-  const cache_key = 'vaults_data';
-  const fetchFunction = async () => {
-    const allAPRs = await axios.get(
-      'https://v3-api.compound.finance/market/all-networks/all-contracts/rewards/dapp-data'
-    );
+export class VaultsService {
+  private redisService: RedisService;
 
-    return allAPRs.data
-      .filter((apr: any) => apr.chain_id === 10)
-      .map((apr: any) => ({
-        comet: apr.comet.address,
-        rewards_apr: apr.earn_rewards_apr,
-        asset: apr.base_asset.address,
-        symbol: apr.base_asset.symbol,
-        decimals: apr.base_asset.decimals,
-        image: tokenImages[apr.base_asset.symbol] || null
-      }));
-  };
-
-  return redisService.getCachedDataWithCallback(cache_key, fetchFunction, 3600);
-}
-
-async function getVaultAPR(vault: any) {
-  try {
-
-    const provider = new JsonRpcProvider(JSON_RPC_PROVIDER);
-    const comet = new Contract(
-      vault.comet,
-      [
-        {
-          inputs: [
-            { internalType: 'uint256', name: 'utilization', type: 'uint256' },
-          ],
-          name: 'getSupplyRate',
-          outputs: [{ internalType: 'uint64', name: '', type: 'uint64' }],
-          stateMutability: 'view',
-          type: 'function',
-        },
-        {
-          inputs: [],
-          name: 'getUtilization',
-          outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-          stateMutability: 'view',
-          type: 'function',
-        }
-      ],
-      provider
-    );
-
-    const utilization = await comet.getUtilization();
-    const supplyRate = await comet.getSupplyRate(utilization);
-    const secondsPerYear = 31536000;
-    const scale = 10 ** 18;
-
-    return ((Number(supplyRate)) / scale) * secondsPerYear;
-  } catch (error: any) {
-    console.error(error.message)
-    throw new Error(error)
+  constructor(redisService: RedisService) {
+    this.redisService = redisService;
   }
-}
 
-async function getVaultBalance(vault: any, account: string) {
-  const cache_key = `vault_balance_${vault.comet}_${account}`;
-  const fetchFunction = async () => {
-    try {
+  private async getVaultsData() {
+    const cache_key = 'vaults_data';
+    const fetchFunction = async () => {
+      const allAPRs = await axios.get(
+        'https://v3-api.compound.finance/market/all-networks/all-contracts/rewards/dapp-data'
+      );
 
-      const provider = new JsonRpcProvider(JSON_RPC_PROVIDER);
-      console.log(vault.comet)
-      const comet = new Contract(
+      return allAPRs.data
+        .filter((apr: any) => apr.chain_id === 10)
+        .map((apr: any) => ({
+          comet: apr.comet.address,
+          rewards_apr: apr.earn_rewards_apr,
+          asset: apr.base_asset.address,
+          symbol: apr.base_asset.symbol,
+          decimals: apr.base_asset.decimals,
+          image: tokenImages[apr.base_asset.symbol] || null
+        }));
+    };
+
+    return this.redisService.getCachedDataWithCallback(cache_key, fetchFunction, 3600);
+  }
+
+  private async getVaultAPR(vault: any) {
+    const cache_key = `vault_apr_${vault.comet}`;
+    const fetchFunction = async () => {
+      try {
+        const provider = new JsonRpcProvider(JSON_RPC_PROVIDER);
+        const comet = new Contract(
         vault.comet,
         [
           {
-            inputs: [{ internalType: 'address', name: 'user', type: 'address' }],
-            name: 'balanceOf',
+            inputs: [
+              { internalType: 'uint256', name: 'utilization', type: 'uint256' },
+            ],
+            name: 'getSupplyRate',
+            outputs: [{ internalType: 'uint64', name: '', type: 'uint64' }],
+            stateMutability: 'view',
+            type: 'function',
+          },
+          {
+            inputs: [],
+            name: 'getUtilization',
             outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
             stateMutability: 'view',
             type: 'function',
@@ -91,45 +66,78 @@ async function getVaultBalance(vault: any, account: string) {
         provider
       );
 
-      console.log({ account })
-      const balance = await comet.balanceOf(account);
-      return balance.toString();
+      const utilization = await comet.getUtilization();
+      const supplyRate = await comet.getSupplyRate(utilization);
+      const secondsPerYear = 31536000;
+      const scale = 10 ** 18;
+
+      return ((Number(supplyRate)) / scale) * secondsPerYear;
     } catch (error: any) {
-      console.error(error)
-      throw new Error(error)
+        console.error(error.message)
+        throw new Error(error)
+      }
+    };
+
+    return this.redisService.getCachedDataWithCallback(cache_key, fetchFunction, 3600);
+  }
+
+  private async getVaultBalance(vault: any, account: string) {
+    const cache_key = `vault_balance_${vault.comet}_${account}`;
+    const fetchFunction = async () => {
+      try {
+        const provider = new JsonRpcProvider(JSON_RPC_PROVIDER);
+        const comet = new Contract(
+          vault.comet,
+          [
+            {
+              inputs: [{ internalType: 'address', name: 'user', type: 'address' }],
+              name: 'balanceOf',
+              outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+              stateMutability: 'view',
+              type: 'function',
+            }
+          ],
+          provider
+        );
+
+        console.log({ account })
+        const balance = await comet.balanceOf(account);
+        return balance.toString();
+      } catch (error: any) {
+        console.error(error)
+        throw new Error(error)
+      }
+    };
+
+    return this.redisService.getCachedDataWithCallback(cache_key, fetchFunction, 3600);
+  }
+
+  public async getVaultsAPR(account: string) {
+    const vaults = await this.getVaultsData();
+
+    const vaultsWithData = await Promise.all(
+      vaults.map(async (vault) => {
+        const [balance, interest_apr] = await Promise.all([
+          this.getVaultBalance(vault, account),
+          this.getVaultAPR(vault)
+        ]);
+
+        return {
+          ...vault,
+          balance: balance.toString(),
+          interest_apr: interest_apr.toString()
+        };
+      })
+    );
+
+    return vaultsWithData;
+  }
+
+  public async refreshVaultsCache(account: string) {
+    const vaults = await this.getVaultsData();
+
+    for (const vault of vaults) {
+      await this.redisService.deleteCachedData(`vault_balance_${vault.comet}_${account}`);
     }
-  };
-
-  return redisService.getCachedDataWithCallback(cache_key, fetchFunction, 3600);
-}
-
-export async function getVaultsAPR(account: string) {
-  const vaults = await getVaultsData();
-
-  // Obtener balances y APRs en paralelo
-  const vaultsWithData = await Promise.all(
-    vaults.map(async (vault) => {
-      const [balance, interest_apr] = await Promise.all([
-        getVaultBalance(vault, account),
-        getVaultAPR(vault)
-      ]);
-
-      return {
-        ...vault,
-        balance: balance.toString(),
-        interest_apr: interest_apr.toString()
-      };
-    })
-  );
-
-  return vaultsWithData;
-}
-
-export async function refreshVaultsCache(account: string) {
-
-  const vaults = await getVaultsData();
-
-  for (const vault of vaults) {
-    await redisService.deleteCachedData(`vault_balance_${vault.comet}_${account}`);
   }
 }
