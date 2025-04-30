230bf6be867f9e92e5b778a99c20bea57e2a0cb6
sebaxor <sebasor@gmail.com>
2025-04-30 01:50:01 -0500

    feat(vaults): format units

--- src/services/vaults.service.ts
+++ src/services/vaults.service.ts
@@ -1,6 +1,6 @@
 import { JSON_RPC_PROVIDER } from '@/config/superChain/constants';
 import axios from 'axios';
-import { Contract, JsonRpcProvider } from 'ethers';
+import { Contract, formatUnits, JsonRpcProvider } from 'ethers';
 import { RedisService } from './redis.service';
 
 const tokenImages = {
@@ -44,35 +44,35 @@ export class VaultsService {
       try {
         const provider = new JsonRpcProvider(JSON_RPC_PROVIDER);
         const comet = new Contract(
-        vault.comet,
-        [
-          {
-            inputs: [
-              { internalType: 'uint256', name: 'utilization', type: 'uint256' },
-            ],
-            name: 'getSupplyRate',
-            outputs: [{ internalType: 'uint64', name: '', type: 'uint64' }],
-            stateMutability: 'view',
-            type: 'function',
-          },
-          {
-            inputs: [],
-            name: 'getUtilization',
-            outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-            stateMutability: 'view',
-            type: 'function',
-          }
-        ],
-        provider
-      );
+          vault.comet,
+          [
+            {
+              inputs: [
+                { internalType: 'uint256', name: 'utilization', type: 'uint256' },
+              ],
+              name: 'getSupplyRate',
+              outputs: [{ internalType: 'uint64', name: '', type: 'uint64' }],
+              stateMutability: 'view',
+              type: 'function',
+            },
+            {
+              inputs: [],
+              name: 'getUtilization',
+              outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+              stateMutability: 'view',
+              type: 'function',
+            }
+          ],
+          provider
+        );
 
-      const utilization = await comet.getUtilization();
-      const supplyRate = await comet.getSupplyRate(utilization);
-      const secondsPerYear = 31536000;
-      const scale = 10 ** 18;
+        const utilization = await comet.getUtilization();
+        const supplyRate = await comet.getSupplyRate(utilization);
+        const secondsPerYear = 31536000;
+        const scale = 10 ** 18;
 
-      return ((Number(supplyRate)) / scale) * secondsPerYear;
-    } catch (error: any) {
+        return ((Number(supplyRate)) / scale) * secondsPerYear;
+      } catch (error: any) {
         console.error(error.message)
         throw new Error(error)
       }
@@ -102,7 +102,8 @@ export class VaultsService {
 
         console.log({ account })
         const balance = await comet.balanceOf(account);
-        return balance.toString();
+        return formatUnits(balance, vault.decimals);
+
       } catch (error: any) {
         console.error(error)
         throw new Error(error)
