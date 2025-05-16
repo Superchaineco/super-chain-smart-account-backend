ffb6d7622ebf734ef9a8d6ca353b13c8763a1181
sebaxor <sebasor@gmail.com>
2025-05-16 16:41:08 -0500

    feat(badges): claim trying to fix nonce error

--- src/services/attestations.service.ts
+++ src/services/attestations.service.ts
@@ -47,9 +47,12 @@ export class AttestationsService {
       value: '0',
       data: data,
     };
-
+    const currentNonce = await safeSdk.getNonce()
     const safeTransaction = await safeSdk.createTransaction({
       transactions: [safeTransactionData],
+      options: {
+        nonce: currentNonce,
+      },
     });
 
     const isValid = await safeSdk.isValidTransaction(safeTransaction, {
