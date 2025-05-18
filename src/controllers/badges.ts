70c81236f457d1f929184a0ae052646326d854b9
sebaxor <sebasor@gmail.com>
2025-05-18 17:21:53 -0500

    feat(badges): claim attestation queue

--- src/controllers/badges.ts
+++ src/controllers/badges.ts
@@ -5,6 +5,7 @@ import { superChainAccountService } from "../services/superChainAccount.service"
 import { isAbleToSponsor } from "../services/sponsorship.service";
 import { AttestationsService } from "../services/attestations.service";
 import { redisService } from "@/services/redis.service";
+import { attestQueueService, AttestQueueService } from "@/services/badges/queue/attestQueue.service";
 export async function getBadges(req: Request, res: Response) {
     const account = req.params.account as string;
     if (!account || account === ZeroAddress) {
@@ -45,16 +46,23 @@ export async function claimBadges(req: Request, res: Response) {
         const badgesService = new BadgesServices();
         const eoas = await superChainAccountService.getEOAS(account);
         const badges = await badgesService.getBadges(eoas, account);
-        const attestationsService = new AttestationsService();
+        //const attestationsService = new AttestationsService();
+        
         const totalPoints = badgesService.getTotalPoints(badges);
         const badgeUpdates = badgesService.getBadgeUpdates(badges);
 
-        const response = await attestationsService.attest(
+        const response = await attestQueueService.queueAndWait({
             account,
             totalPoints,
             badges,
             badgeUpdates,
-        );
+        });
+        // const response = await attestationsService.attest(
+        //     account,
+        //     totalPoints,
+        //     badges,
+        //     badgeUpdates,
+        // );
         const cacheKey = `user_badges:${account}`;
         await redisService.deleteCachedData(cacheKey);
 
---
+++ src/services/badges/queue/attestQueue.service.ts
@@ -0,0 +1,83 @@
+
+import { Job, Queue, QueueEvents, Worker } from 'bullmq';
+import { redisWorker } from '@/utils/cache';
+import { ENV, ENVIRONMENTS } from '@/config/superChain/constants';
+import { AttestationsService } from '@/services/attestations.service';
+import { ResponseBadge } from '../badges.service';
+
+
+interface AttestJobData {
+    account: string;
+    totalPoints: number;
+    badges: ResponseBadge[];
+    badgeUpdates: { badgeId: number; level: number; points: number }[];
+}
+
+export class AttestQueueService {
+    private readonly queueName = 'attestQueue';
+    public readonly queue: Queue<AttestJobData>;
+    private readonly queueEvents: QueueEvents;
+    private readonly worker?: Worker<AttestJobData>;
+
+    constructor() {
+        this.queue = new Queue(this.queueName, {
+            connection: redisWorker,
+        });
+
+        this.queueEvents = new QueueEvents(this.queueName, {
+            connection: redisWorker,
+        });
+
+        if (ENV === ENVIRONMENTS.production) {
+            this.worker = new Worker(
+                this.queueName,
+                async (job) => {
+                    const { account, totalPoints, badges, badgeUpdates } = job.data;
+                    const service = new AttestationsService();
+                    console.log('Executing attestation ', account);
+                    return await service.attest(account, totalPoints, badges, badgeUpdates);
+                },
+                {
+                    concurrency: 1, // garantizamos ejecución única
+                    connection: redisWorker,
+                }
+            );
+
+            this.worker.on('error', (err) => {
+                console.error('[Attest Worker Error]', err);
+            });
+        }
+    }
+
+    public async queueAndWait(data: AttestJobData): Promise<any> {
+        const jobId = `attest-${data.account}`;
+        console.log('Job ID:', jobId);
+        const existing = await this.queue.getJob(jobId);
+        if (
+            existing &&
+            !(await existing.isCompleted()) &&
+            !(await existing.isFailed())
+        ) {
+            await existing.remove();
+        }
+
+        const job = await this.queue.add(this.queueName, data, {
+            jobId,
+            attempts: 3,
+            backoff: {
+                type: 'exponential',
+                delay: 1000,
+            },
+            removeOnComplete: {
+                age: 3600,
+            },
+            removeOnFail: {
+                age: 86400,
+            },
+        });
+        console.log('Waiting...', jobId);
+
+        return await job.waitUntilFinished(this.queueEvents);
+    }
+}
+export const attestQueueService = new AttestQueueService();
