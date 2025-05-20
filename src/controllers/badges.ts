1e8745d333e3ec7d7ac6480cf33efa99bc0b2968
sebaxor <sebasor@gmail.com>
2025-05-20 12:33:37 -0500

    feat(captcha): captcha for badge claiming priority

--- src/controllers/badges.ts
+++ src/controllers/badges.ts
@@ -6,7 +6,7 @@ import { isAbleToSponsor } from '../services/sponsorship.service';
 import { AttestationsService } from '../services/attestations.service';
 import { redisService } from '@/services/redis.service';
 import { attestQueueService, AttestQueueService } from "@/services/badges/queue/attestQueue.service";
-import { TURNSTILE_SITE_KEY } from '@/config/superChain/constants';
+import { TURNSTILE_SECRET_KEY } from '@/config/superChain/constants';
 export async function getBadges(req: Request, res: Response) {
   const account = req.params.account as string;
   if (!account || account === ZeroAddress) {
@@ -43,22 +43,23 @@ export async function claimBadges(req: Request, res: Response) {
     //     return res.status(500).json({ error: "User is not able to sponsor" });
     // }
 
-    const tokenFromFrontend = req.body.tokenHumans;
+    const tokenFromFrontend = req.body.captchaToken;
     const ipAddress = req.ip;
 
-    const res = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
+    const captchaRes = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams({
-        secret: TURNSTILE_SITE_KEY!,
+        secret: TURNSTILE_SECRET_KEY!,
         response: tokenFromFrontend,
         remoteip: ipAddress,
       }),
     });
 
-    const data = await res.json();
+    
+    const data = await captchaRes.json();
     const isHuman = data.success
-
+    console.log("Captcha response", data);
 
     const badgesService = new BadgesServices();
     const eoas = await superChainAccountService.getEOAS(account);
