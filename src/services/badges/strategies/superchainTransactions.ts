1d43e41eb53bd9bda8170070cf64ce2836d762e1
sebaxor <sebasor@gmail.com>
2025-05-19 20:58:00 -0500

    feat(badges): base badge SOT  changed to alchemy

--- src/services/badges/strategies/superchainTransactions.ts
+++ src/services/badges/strategies/superchainTransactions.ts
@@ -1,5 +1,7 @@
+import { Alchemy, AssetTransfersCategory, Network } from "alchemy-sdk";
 import { BaseBadgeStrategy } from "./badgeStrategy";
 import { Season } from "@/types/index.types";
+import { redisService } from "@/services/redis.service";
 
 
 
@@ -33,10 +35,11 @@ export class SuperChainTransactionsStrategy extends BaseBadgeStrategy {
     }
     async getValue(eoas: string[]) {
         const season = this.getSeason();
-        
+
         const requests: Promise<number>[] = [
             this.getCachedSeasonedValue({ service: "blockscout", chain: "optimism-10", chainId: "10", eoas, season }),
-            this.getCachedSeasonedValue({ service: "blockscout", chain: "base-8453", chainId: "8453", eoas, season }),
+            // this.getCachedSeasonedValue({ service: "blockscout", chain: "base-8453", chainId: "8453", eoas, season }),
+            this.getBase(eoas),
             this.getCachedSeasonedValue({ service: "blockscout", chain: "ink-57073", chainId: "57073", eoas, season }),
             this.getCachedSeasonedValue({ service: "blockscout", chain: "unichain-130", chainId: "130", eoas, season }),
             this.getCachedSeasonedValue({ service: "blockscout", chain: "Soneium", chainId: "", eoas, season }),
@@ -47,6 +50,47 @@ export class SuperChainTransactionsStrategy extends BaseBadgeStrategy {
         const totalTxs: number = results.reduce((acc, curr) => acc + curr, 0);
 
         return totalTxs;
+    }
+
+    async getBase(eoas: string[]): Promise<number> {
+        const cacheKey = `baseTransactions-S7-${eoas.join(",")}`;
+        const season = this.getSeason();
+
+        const fetchFunction = async () => {
+            const settings = {
+                apiKey: process.env.ALCHEMY_PRIVATE_KEY!,
+                network: Network.BASE_MAINNET,
+            };
+            const alchemy = new Alchemy(settings);
+            const transactions = await eoas.reduce(async (accPromise, eoa) => {
+                const acc = await accPromise;
+
+                const toBlock = Date.now() >= new Date(2025, 5, 11).getTime()
+                    ? season.blockRanges["base-8453"][1]
+                    : null;
+                const outgoingResult = await alchemy.core.getAssetTransfers({
+                    fromBlock: season.blockRanges["base-8453"][0],
+                    toBlock: toBlock,
+                    fromAddress: eoa,
+                    excludeZeroValue: false,
+                    category: [AssetTransfersCategory.EXTERNAL, AssetTransfersCategory.ERC20, AssetTransfersCategory.ERC1155]
+                });
+
+                const incomingResult = await alchemy.core.getAssetTransfers({
+                    fromBlock: season.blockRanges["base-8453"][0],
+                    toBlock: toBlock,
+                    toAddress: eoa,
+                    excludeZeroValue: false,
+                    category: [AssetTransfersCategory.EXTERNAL, AssetTransfersCategory.ERC20, AssetTransfersCategory.ERC1155]
+                });
+
+                return acc + outgoingResult.transfers.length + incomingResult.transfers.length;
+            }, Promise.resolve(0));
+
+            return transactions;
+        };
+
+        return redisService.getCachedDataWithCallback(cacheKey, fetchFunction, 3600);
     }
 
 }
