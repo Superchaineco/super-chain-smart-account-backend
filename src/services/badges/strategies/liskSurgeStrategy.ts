29f2282a7222f6f3d170b188fdd056fc6f2163ec
sebaxor <sebasor@gmail.com>
2025-05-20 00:56:11 -0500

    feat(badges): lisk fix

--- src/services/badges/strategies/liskSurgeStrategy.ts
+++ src/services/badges/strategies/liskSurgeStrategy.ts
@@ -8,18 +8,19 @@ export class LiskSurgeStrategy extends BaseBadgeStrategy {
     campaigns: string[] = ["Lisk Surge"]
 
     async getValue(eoas: string[]): Promise<number> {
-        
+
 
         const amount = eoas.reduce(async (accPromise, eoa) => {
             const urlGet = `https://blockscout.lisk.com/api?&module=account&action=txlist&address=${eoa}`
             const queueService = getBadgesQueue('blockscout')
             const response = await queueService.getCachedDelayedResponse(urlGet);
-            const total = response.result
+
+            const total = response?.result
                 .filter(tx => tx.from.toLowerCase() === '0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae'.toLowerCase())
                 .reduce((acc: number, tx) => {
                     const value = Number(tx.value) / Math.pow(10, 18);
                     return acc + value;
-                }, 0);
+                }, 0) ?? 0;
             return (await accPromise) + total;
         }, Promise.resolve(0));
 
