7a9904847f650aba2e6a7fab3e5c6c6133e50a26
sebaxor <sebasor@gmail.com>
2025-06-09 16:51:30 -0500

    feat(badges): adding lil nouns and base nouns strategies

---
+++ src/services/badges/strategies/lilNounsCheckStrategy.ts
@@ -0,0 +1,30 @@
+import { BaseBadgeStrategy } from "./badgeStrategy";
+import { ethers } from "ethers";
+import { redisService } from "../../redis.service";
+
+export class LilNounsCheckStrategy extends BaseBadgeStrategy {
+  async getValue(eoas: string[]): Promise<number> {
+    const cacheKey = `hasNouns-${eoas.join(",")}`;
+    const ttl = 86400;
+
+    const fetchFunction = async () => {
+      const provider = new ethers.JsonRpcProvider(process.env.JSON_RPC_ETH_PROVIDER);
+      const contract = new ethers.Contract(
+        process.env.LIL_NOUNS_CONTRACT_ADDRESS!,
+        ["function balanceOf(address owner) public view returns (uint256)"],
+        provider,
+      );
+
+      let countNouns: bigint = BigInt(0); 
+
+      for (const eoa of eoas) {
+        const balance: bigint = await contract.balanceOf(eoa); 
+        countNouns += balance;
+      }
+
+      return Number(countNouns); 
+    };
+
+    return redisService.getCachedDataWithCallback(cacheKey, fetchFunction, ttl);
+  }
+}
