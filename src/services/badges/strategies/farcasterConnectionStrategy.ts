68b52cfa3c4fdd43a026a5452efb90a0f770a25c
sebaxor <sebasor@gmail.com>
2025-06-26 23:48:37 -0500

    fix farcaster badge

--- src/services/badges/strategies/farcasterConnectionStrategy.ts
+++ src/services/badges/strategies/farcasterConnectionStrategy.ts
@@ -10,7 +10,7 @@ export class FarcasterConnectionStrategy extends BaseBadgeStrategy {
     async getValue(eoas: string[]): Promise<boolean> {
         const cacheKey = `farcasterLink-${eoas.join(',')}`;
         const data = await redisService.getCachedData(cacheKey)
-        return data?.signature.length > 0 ? true : false;
+        return data != null ? true : false;
     }
 
 
