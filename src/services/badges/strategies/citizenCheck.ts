0ee6da4a839dd7b69f02d1f656dfbcb1606e9a85
anotherdev <joseluismanco37@gmail.com>
2025-07-08 12:03:03 -0500

    feat: update citizen SoT

--- src/services/badges/strategies/citizenCheck.ts
+++ src/services/badges/strategies/citizenCheck.ts
@@ -1,42 +1,57 @@
-import { BaseBadgeStrategy } from "./badgeStrategy";
-import fs from "fs";
-import csv from "csv-parser";
-
+import { BaseBadgeStrategy } from './badgeStrategy';
+import fs from 'fs';
+import csv from 'csv-parser';
+import axios from 'axios';
 
 type CsvRow = {
   Address: string;
   ENS: string;
 };
 
-
-const CitizenFilePath = "src/data/citizen.csv";
+const CitizenFilePath = 'src/data/citizen.csv';
 
 export class CitizenCheckStrategy extends BaseBadgeStrategy {
+  // private async loadCsvData(filePath: string): Promise<CsvRow[]> {
+  //     return new Promise((resolve, reject) => {
+  //         const results: CsvRow[] = [];
+  //         fs.createReadStream(filePath)
+  //             .pipe(csv())
+  //             .on("data", (data: CsvRow) => results.push(data))
+  //             .on("end", () => resolve(results));
+  //     });
+  // }
 
+  async getValue(eoas: string[]): Promise<boolean> {
+    const citizenApiUrl = 'https://optimism.easscan.org/graphql';
 
-    private async loadCsvData(filePath: string): Promise<CsvRow[]> {
-        return new Promise((resolve, reject) => {
-            const results: CsvRow[] = [];
-            fs.createReadStream(filePath)
-                .pipe(csv())
-                .on("data", (data: CsvRow) => results.push(data))
-                .on("end", () => resolve(results));
-        });
-    }
-
-    async getValue(eoas: string[]): Promise<boolean> {
-        const csvData = await this.loadCsvData(CitizenFilePath);
-
-        for (const eoa of eoas) {
-            const citizen = csvData.find((row) =>
-                row.Address
-                    ? row.Address.toLocaleLowerCase() === eoa.toLowerCase()
-                    : false,
-            );
-            if (citizen) {
-                return true;
-            }
+    const citizenQuery = `
+    query AddressGivethDonations($addresses: [String!]!) {
+      attestations(
+        take: 10,
+        where: {
+          schemaId: {
+            equals: "0xc35634c4ca8a54dce0a2af61a9a9a5a3067398cb3916b133238c4f6ba721bc8a"
+          }
+          recipient: {
+            in: $addresses
+          }
         }
-        return false;
-    }
+      ) {
+        schemaId
+        recipient
+        data
+      }
+    }`;
+
+    const response = await axios.post(citizenApiUrl, {
+      query: citizenQuery,
+      variables: {
+        addresses: eoas,
+      },
+    });
+
+    if (response.data.data.attestations.length > 0) return true;
+
+    return false;
+  }
 }
