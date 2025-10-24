61442a0d05d0753ecd5d1aaf82fc649fab5f772e
anotherdev <joseluismanco37@gmail.com>
2025-10-23 21:20:31 -0500

    chore: graphcliet

--- .graphclient/index.ts
+++ .graphclient/index.ts
@@ -2613,7 +2613,7 @@ const superAccountsTransforms = [];
 const additionalTypeDefs = [] as any[];
 const superAccountsHandler = new GraphqlHandler({
               name: "super-accounts",
-              config: {"endpoint":"https://api.studio.thegraph.com/query/72352/super-accounts-staging/version/latest"},
+              config: {"endpoint":"https://api.studio.thegraph.com/query/72352/super-accounts-staging/v1.2.2"},
               baseDir,
               cache,
               pubsub,
