df6af9eb04f697c7464ad033a7a467a050dda438
sebaxor <sebasor@gmail.com>
2026-01-06 13:56:00 -0500

    tune: pool params

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,9 +6,9 @@ dotenv.config();
 export const pgPool = new Pool({
     connectionString: DATABASE_URL,
     ssl: { rejectUnauthorized: false },
-    max: Number(process.env.PG_POOL_MAX ?? 10),
-    idleTimeoutMillis: 30_000,
-    connectionTimeoutMillis: 5_000, 
+    max: Number(process.env.PG_POOL_MAX ?? 50),
+    idleTimeoutMillis: 300_000,
+    connectionTimeoutMillis: 300_000, 
 });
 pgPool.on('connect', (c) => {
     c.query("SET statement_timeout='30s'; SET idle_in_transaction_session_timeout='30s';").catch(() => { });
